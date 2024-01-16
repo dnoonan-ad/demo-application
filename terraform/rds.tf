8db87a2ddccf99a7becf3fa2cb3d0a10d29f45d5
davidnoonan <david_noonan@outlook.com>
2024-01-16 10:25:38 +0000

    initial commit

---
+++ terraform/rds.tf
@@ -0,0 +1,42 @@
+module "db" {
+  source = "terraform-aws-modules/rds/aws"
+
+  identifier = "postgres_db"
+
+  engine               = "postgres"
+  engine_version       = "14"
+  family               = "postgres14" # DB parameter group
+  major_engine_version = "14"         # DB option group
+  instance_class       = "db.t4g.large"
+
+  db_name  = "AppPostgresql"
+  username = "app_postgresql"
+  port     = 5432
+
+  db_subnet_group_name   = module.vpc.database_subnet_group
+  vpc_security_group_ids = [module.security_group.security_group_id]
+
+  tags = local.tags
+}
+
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.0"
+
+  name        = local.name
+  description = "Postgres Security Group"
+  vpc_id      = module.vpc.vpc_id
+
+  # ingress
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 5432
+      to_port     = 5432
+      protocol    = "tcp"
+      description = "PostgreSQL access from within VPC"
+      cidr_blocks = module.vpc.vpc_cidr_block
+    },
+  ]
+
+  tags = local.tags
+}
